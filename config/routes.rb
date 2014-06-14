b15e18504adcb53287e0e9882f77dccaaad3de19
Johnathans <johnathan.smith@g5searchmarketing.com>
2014-06-14 16:28:16 -0700

    Updated views for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pom2::Application.routes.draw do
+  devise_for :users
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
