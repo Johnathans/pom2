630898557ea1e02868a4ce52aa5597c53db98f67
Johnathans <johnathan.smith@g5searchmarketing.com>
2014-06-14 12:01:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pom2::Application.config.secret_key_base = '3d044aa5b3dc6a27cd2ee21d4b74ddf4f9cc04fa40777ef36689a4889713ef1f99cec23b2676a85c9607f8a853603c92114f2cce5a0efcdf15619df2245a778f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pom2::Application.config.session_store :cookie_store, key: '_pom2_session'
